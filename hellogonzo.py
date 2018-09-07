e07cf904eb272e2580515af3b3a6f4f1a2af5b98
shaynesharpe <shayne.sharpe@dbca.wa.gov.au>
2018-09-07 11:41:30 +0800

    initial gonzo files for django

---
+++ hellogonzo.py
@@ -0,0 +1,13 @@
+showMessage = True
+showHelloGonzo = True
+
+
+if showMessage:
+    print("This message should be shown")
+
+    if showHelloGonzo:
+        print("Hello Gonzo")
+
+
+print ("Thanks for running this script Gonzo!")
+
